51e78e5971a92efe361057ebfd168413b43fd68b
YaseminDenizOzturk <yasmindeniz188@gmail.com>
2023-08-07 09:17:28 +0300

    upload

---
+++ degiskenler/src/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int tamSayi;
+    float kesirliSayi;
+    double kesirliSayi2;
+    char karakter;
+    bool anahtar;
+
+    tamSayi = 12;
+    kesirliSayi = 5.5;
+    kesirliSayi2 = 3.14;
+    karakter = 'Y';
+    anahtar = true;
+
+    cout << "Tamsayi: " << tamSayi << endl<< "Kesirli Sayi: " << kesirliSayi << endl;
+    cout<<tamSayi<<endl<<kesirliSayi<<endl<<kesirliSayi2<<endl<<karakter<<endl<<anahtar;
+    
+    return 0;
+}

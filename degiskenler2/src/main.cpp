cbd56bf94bbb90b09824bca430daf06b9e61317c
YaseminDenizOzturk <yasmindeniz188@gmail.com>
2023-08-07 21:01:08 +0300

    upload

---
+++ degiskenler2/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int sayi1 = 10;
+    double sayi2 = 12.8;
+    cout << sayi1 + sayi2 << endl;
+    
+    return 0;
+}
---
+++ kullanicidan_veri_alma/src/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int tamsayi;
+    float kesirlisayi;
+    char karakter;
+    bool anahtar;
+
+    cout<<"Lutfen bir tam sayi giriniz"<<endl;
+    cin>>tamsayi;
+
+    cout<<"Lutfen bir kesirli sayi giriniz"<<endl;
+    cin>>kesirlisayi;
+
+    cout<<"Lutfen bir karakter giriniz"<<endl;
+    cin>>karakter;
+
+    cout<<"Lutfen bir anahtar giriniz"<<endl;
+    cin>>anahtar;
+
+    cout<<"Kullanicinin girdiği tamsayi: "<<tamsayi<<endl;
+    cout<<"Kullanicinin girdiği kesirlisayi: "<<kesirlisayi<<endl;
+    cout<<"Kullanicinin girdiği karakter: "<<karakter<<endl;
+    cout<<"Kullanicinin girdiği anahtar: "<<anahtar<<endl;
+
+}
+

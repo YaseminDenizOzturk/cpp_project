193719791c95e87e95c227fa02e20d03390f1e01
YaseminDenizOzturk <yasmindeniz188@gmail.com>
2023-08-12 12:51:23 +0300

    upload

---
+++ daire_cevre_alan_hesabi/src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float pi = 3.14;
+    float r;
+    cout<< "Lutfen dairenin yaricapini giriniz: ";
+    cin>>r;
+
+    cout<< "Dairenin alani: "<<pi * r *r <<endl;
+    cout<< "Dairenin cevresi: "<<2 * pi * r <<endl;
+
+    return 0;  
+
+}
+ 
---
+++ kare_alan_cevre_hesabi/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"Lutfen karenin bir kenarini giriniz: ";
+    cin>>a;
+
+    cout<<"Karenin Alani: "<<a *a<<endl;
+    cout<<"Karenin Cevresi: "<<4 *a<<endl;
+
+    return 0;
+}

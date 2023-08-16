885ef11d2fb067bca06a26cda81c7db91e3a3180
YaseminDenizOzturk <yasmindeniz188@gmail.com>
2023-08-16 23:20:56 +0300

    upload

---
+++ for_dongusu/src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 0;
+    int j = 10;
+
+    for(i = 0;i < 11 && j == 10;i++){
+        cout<<i<<endl; 
+    }
+    return 0;
+}

b66a947f8f6f8bc167566e48b3d2d26731bd4bf4
YaseminDenizOzturk <yasmindeniz188@gmail.com>
2023-08-18 14:34:23 +0300

    upload

---
+++ break_continue_for_dongusu2/src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int j = 0;
+
+    for(;j < 51;j++){
+        if(j == 26){
+            break;
+        }
+        cout<<j<<endl;
+    }
+    return 0;
+}
+
+// for dongusunde sonsuz dongu yapmak için
+    // for(;;){
+    //     cout<<5+1<<endl;
+    // }
+    
